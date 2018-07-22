d52b29fcab131177937b986623748eb188a81882
FinchyG <lagaunt120@yahoo.com>
2018-07-22 15:03:12 +0100

    switch to using css() method

--- jQuery.js
+++ jQuery.js
@@ -1,91 +1,91 @@
 $('document').ready(function() {
 
   $('#show1').click(function() {
-    $('#a1').show();
+    $('#a1').css('visibility', 'visible');
   });
 
   $('#hide1').click(function() {
-    $('#a1').hide();
+    $('#a1').css('visibility', 'hidden');
   });
   
   $('#show2').click(function() {
-    $('#a2').show();
+    $('#a2').css('visibility', 'visible');
   });
 
   $('#hide2').click(function() {
-    $('#a2').hide();
+    $('#a2').css('visibility', 'hidden');
   });
   
   $('#show3').click(function() {
-    $('#a3').show();
+    $('#a3').css('visibility', 'visible');
   });
 
   $('#hide3').click(function() {
-    $('#a3').hide();
+    $('#a3').css('visibility', 'hidden');
   });
   
   $('#show4').click(function() {
-    $('#a4').show();
+    $('#a4').css('visibility', 'visible');
   });
 
   $('#hide4').click(function() {
-    $('#a4').hide();
+    $('#a4').css('visibility', 'hidden');
   });
   
   $('#show5').click(function() {
-    $('#a5').show();
+    $('#a5').css('visibility', 'visible');
   });
 
   $('#hide5').click(function() {
-    $('#a5').hide();
+    $('#a5').css('visibility', 'hidden');
   });
   
   $('#show6').click(function() {
-    $('#a6').show();
+    $('#a6').css('visibility', 'visible');
   });
 
   $('#hide6').click(function() {
-    $('#a6').hide();
+    $('#a6').css('visibility', 'hidden');
   });
   
   $('#show7').click(function() {
-    $('#a7').show();
+    $('#a7').css('visibility', 'visible');
   });
 
   $('#hide7').click(function() {
-    $('#a7').hide();
+    $('#a7').css('visibility', 'hidden');
   });
   
   $('#show8').click(function() {
-    $('#a8').show();
+    $('#a8').css('visibility', 'visible');
   });
 
   $('#hide8').click(function() {
-    $('#a8').hide();
+    $('#a8').css('visibility', 'hidden');
   });
   
   $('#show9').click(function() {
-    $('#a9').show();
+    $('#a9').css('visibility', 'visible');
   });
 
   $('#hide9').click(function() {
-    $('#a9').hide();
+    $('#a9').css('visibility', 'hidden');
   });
   
   $('#show10').click(function() {
-    $('#a10').show();
+    $('#a10').css('visibility', 'visible');
   });
 
   $('#hide10').click(function() {
-    $('#a10').hide();
+    $('#a10').css('visibility', 'hidden');
   });
 
   $('#showAll').click(function() {
-    $('#a1, #a2, #a3, #a4, #a5, #a6, #a7, #a8, #a9, #a10').show();
+    $('#a1, #a2, #a3, #a4, #a5, #a6, #a7, #a8, #a9, #a10').css('visibility', 'visible');
   });
 
   $('#hideAll').click(function() {
-    $('#a1, #a2, #a3, #a4, #a5, #a6, #a7, #a8, #a9, #a10').hide();
+    $('#a1, #a2, #a3, #a4, #a5, #a6, #a7, #a8, #a9, #a10').css('visibility', 'hidden');;
   });
 
 });
